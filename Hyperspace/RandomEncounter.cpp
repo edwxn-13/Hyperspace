a59b716c3d6f5bcae6cb2cf20c72f119557a5baf
edwxn-13 <73799683+edwxn-13@users.noreply.github.com>
2021-03-10 00:27:51 +0000

    l

--- Hyperspace/RandomEncounter.cpp
+++ Hyperspace/RandomEncounter.cpp
@@ -1,1 +1,24 @@
 #include "RandomEncounter.h"
+RandomEncounter::RandomEncounter(int threatVal) 
+{
+  threat = threatVal;
+}
+
+GamePackage RandomEncounter::Combat(GamePackage gamePackage)
+{
+  for (int i = 0; i < rand() % 5; i++)
+  {
+    nEnemyContainer.push_back(Enemy(threat));
+  }
+  return gamePackage;
+  bool encounterStatus = true;
+  while (encounterStatus) 
+  {
+    int pTargetChoice;
+    int pWeaponChoice;
+    int pSystemChoice;
+    int eTargetChoice;
+    int eWeaponChoice;
+    int eSystemChoice;
+  }
+}
--- Hyperspace/RandomEncounter.h
+++ Hyperspace/RandomEncounter.h
@@ -1,11 +1,14 @@
 #pragma once
+#include "GamePackage.h"
+#include "Enemy.h"
 class RandomEncounter 
 {
 public:
   RandomEncounter(int);
-
+  GamePackage Combat(GamePackage gamePackage);
 private:
   int threat;
-  
+  std::vector<Enemy> nEnemyContainer;
+
 };
 
